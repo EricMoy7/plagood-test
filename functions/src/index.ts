7cd6e7fb52624838dd4f74a1965b1819af26f2ec
Eric <ericzmoy@gmail.com>
2021-08-10 13:52:17 -0500

    minor type fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,42 +3,46 @@ import firebase from './firebase';
 
 const db = firebase.database(); //Same thing as admin
 
-const getUserData = functions.https.onCall(async (data, context) => {
-	//Need to verify user from context.auth
-	//uid can be grabbed from context as well
+const getUserData = functions.https.onCall(
+	async (data: { id: string }, context) => {
+		//Need to verify user from context.auth
 
-	//This is for unit testing with firebase functions shell
-	const { id } = data;
+		//uid can be grabbed from context on production
+		//const {uid} = context;
 
-	try {
-		//Get users from myPosts path
-		const snapUser = await db.ref(`/myPosts/${id}/`).get();
-		if (snapUser.exists()) {
-			const snapListObject = snapUser.val();
+		//This is for unit testing with firebase functions shell
+		const { id } = data;
 
-			//Initalize userPosts lists for return
-			let userPosts: any[] = [];
+		try {
+			//Get users from myPosts path
+			const snapUser = await db.ref(`/myPosts/${id}/`).get();
+			if (snapUser.exists()) {
+				const snapListObject = snapUser.val();
 
-			//Loop through user posts and append to userPosts array
-			for (let post in snapListObject) {
-				if (snapListObject[post] === true) {
-					const snapPost = await db.ref(`posts/${post}`).get();
-					if (snapPost.exists()) {
-						userPosts = [...userPosts, snapPost.val()];
+				//Initalize userPosts lists for return
+				let userPosts: any[] = [];
+
+				//Loop through user posts and append to userPosts array
+				for (let post in snapListObject) {
+					if (snapListObject[post] === true) {
+						const snapPost = await db.ref(`posts/${post}`).get();
+						if (snapPost.exists()) {
+							userPosts = [...userPosts, snapPost.val()];
+						}
 					}
 				}
-			}
 
-			return userPosts;
-		} else {
-			console.log('User data not availible');
-			return 'User data not availible';
+				return userPosts;
+			} else {
+				console.log('User data not availible');
+				return 'User data not availible';
+			}
+		} catch (error) {
+			console.log(`Error: ${error}`);
+			return error;
 		}
-	} catch (error) {
-		console.log(`Error: ${error}`);
-		return error;
 	}
-});
+);
 
 //This was an attempt at Promising chaining, but just makes the code too complex for one function
 //This could possible be used for asynchronous lazy loading if the front end allows
